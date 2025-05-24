39863a2e20ff57bf62fcdc79a225f4cd4b9c71fc
Rodolfo <rodoaleguerragar13@gmail.com>
2025-05-24 14:00:32 -0600

    big_personalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+//Variables
+const dwnld_btn = document.querySelector("#dwnld_resume");
+
+//Functions
+function printResume() {
+  window.print();
+}
+
+//Listeners
+dwnld_btn.addEventListener("click", printResume);
